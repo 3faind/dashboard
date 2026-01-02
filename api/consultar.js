c53133bdc181d6c1d4ba1c6116ea928e733bcd73
3faind <3facosmeticos@gmail.com>
2026-01-02 16:45:59 -0300

    Update consultar.js

--- api/consultar.js
+++ api/consultar.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
     }
 
     // const urlFinal = `${BASE_URL}/${endpoint}${queryParams}`;
-    const urlFinal = `${BASE_URL}/${endpoint}${queryParams};pagina=1`;
+    const urlFinal = `${BASE_URL}/${endpoint}${queryParams};pagina=2`;
 
     const response = await fetch(urlFinal, {
       method: "GET",
@@ -41,5 +41,6 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: error.message });
   }
 }
+
 
 
